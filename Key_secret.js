46ea2b74f10eed735117a4c7ec341f12e22a26ca
MiterX <you@example.com>
2018-03-08 18:19:23 +0100

    make it batter

---
+++ Key_secret.js
@@ -0,0 +1,8 @@
+module.exports = {
+  consumer_key:         '490meHmrsPhAmujodCo0jmglq',
+  consumer_secret:      'Ln4kekfLGPKpMQavQGxLRpgnIMJbiper7TlWbrZ6pfzTbynkAL',
+  access_token:         '114416621-2nFvwBXHudQeyXZFKrD6CdoDJrQtOmnSqQR9FxAd',
+  access_token_secret:  'iN420ZUyRqy8mkcBzbAe6FKA5RSIrsWgYdcoGIWzJMxmu',
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
+
