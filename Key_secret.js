8d448a31c16c0ed120a21a7acbcece9c1a1f5b4e
3d4K9ZlAUUic <33258239+SevenGoldenHorses@users.noreply.github.com>
2018-04-30 16:39:32 +0200

    Update Key_secret.js

--- Key_secret.js
+++ Key_secret.js
@@ -1,8 +1,8 @@
 module.exports = {
   consumer_key:         '490meHmrsPhAmujodCo0jmglq',
   consumer_secret:      'Ln4kekfLGPKpMQavQGxLRpgnIMJbiper7TlWbrZ6pfzTbynkAL',
-  access_token:         '114416621-2nFvwBXHudQeyXZFKrD6CdoDJrQtOmnSqQR9FxAd',
-  access_token_secret:  'iN420ZUyRqy8mkcBzbAe6FKA5RSIrsWgYdcoGIWzJMxmu',
+  access_token:         '114416621-GsZKjBZTZsXzta8dyhxCrcj7PScfWs52iYimnCTQ',
+  access_token_secret:  'qM57jqTtJN5PEhdWz6n6YDv5m405sfXiaJ6XHFI6Gk7hP',
   // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
 
